f0987b8b1107f4a619975a203d80a959101630cf
cdyzh <cindyzhangty@hotmail.com>
2024-08-15 00:49:23 -0400

    user can upload variant ID only; added help button

--- app.R
+++ app.R
@@ -12,12 +12,19 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      checkboxInput("upload_file", "Upload Your Own CSV File", value = FALSE),
+      actionButton("upload_guide", "Upload Dataset Guide", class = "btn-primary"),
+      selectInput("upload_type", "Select Upload Option:",
+                  choices = list("Full Dataset (All Columns)" = "full", "Gene and Variant ID Only" = "gene_variant"),
+                  selected = "full"),
       conditionalPanel(
-        condition = "input.upload_file == true",
-        fileInput("file", "Upload CSV File", 
-                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
-        actionButton("infoButton", "i", class = "btn-info")
+        condition = "input.upload_type == 'full'",
+        fileInput("file_full", "Upload Full CSV File", 
+                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"))
+      ),
+      conditionalPanel(
+        condition = "input.upload_type == 'gene_variant'",
+        fileInput("file_gene_variant", "Upload Gene and Variant ID CSV File", 
+                  accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv"))
       ),
       selectizeInput("gene", "Select Gene Name:", choices = NULL, options = list(maxOptions = 1000)),
       checkboxInput("common_variant_filter", "Exclude Common Variants (gnomAD AF > 0.005)", value = TRUE),
@@ -33,7 +40,8 @@ ui <- fluidPage(
       downloadButton("downloadPlotPNG", "Download PRC Plot as PNG"),
       downloadButton("downloadPlotPDF", "Download PRC Plot and Metadata"),
       helpText("Example genes: LDLR, TTN (no VARITY), ACADVL, DNM2, MYH7, SCN5A, etc."),
-      helpText("If error occurs, it means there is not enough data to generate the PRC. Try using fewer predictors.")
+      helpText("If error occurs, it means there is not enough data to generate the PRC. Try using fewer predictors."), 
+      actionButton("helpButton", "Help (Q&A)", class = "btn-warning")
     ),
     
     mainPanel(
@@ -56,10 +64,22 @@ server <- function(input, output, session) {
   )
   
   prcdata <- reactive({
-    if (input$upload_file) {
-      req(input$file)
-      df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
+    if (input$upload_type == "full" && !is.null(input$file_full)) {
+      req(input$file_full)
+      df <- read.csv(input$file_full$datapath, stringsAsFactors = FALSE)
       colnames(df) <- standard_colnames
+      df
+    } else if (input$upload_type == "gene_variant" && !is.null(input$file_gene_variant)) {
+      req(input$file_gene_variant)
+      df <- read.csv(input$file_gene_variant$datapath, stringsAsFactors = FALSE)
+      
+      # Load the full stored dataset for matching
+      full_df <- read.csv("preprocessed_id.csv", stringsAsFactors = FALSE)
+      colnames(df) <- c("base__hugo", "base__achange")
+      
+      # Merge based on gene and variant ID
+      df <- merge(df, full_df, by = c("base__hugo", "base__achange"))
+      print(df)
       df
     } else {
       read.table("preprocessed_id.csv", sep = ',', header = TRUE, stringsAsFactors = FALSE)
@@ -193,6 +213,42 @@ server <- function(input, output, session) {
       }
     }
   )
+  
+  observeEvent(input$upload_guide, {
+    showModal(modalDialog(
+      title = "Upload Dataset Guide",
+      HTML("<h4>Option 1: Full Dataset (All Columns)</h4>
+            <p>Upload a CSV file with the following columns:</p>
+            <ul>
+              <li><b>gene:</b> Gene name</li>
+              <li><b>gnomad_af:</b> gnomAD allele frequency</li>
+              <li><b>varity_r:</b> VARITY score</li>
+              <li><b>alphamissense:</b> AlphaMissense score</li>
+              <li><b>revel:</b> REVEL score</li>
+              <li><b>classification:</b> TRUE for Pathogenic, FALSE for Benign</li>
+            </ul>
+            <h4>Option 2: Gene and Variant ID Only</h4>
+            <p>Upload a CSV file with the following columns:</p>
+            <ul>
+              <li><b>gene:</b> Gene name</li>
+              <li><b>variant_id:</b> Variant ID</li>
+            </ul>
+            <p>The data will be matched with the stored dataset to retrieve corresponding scores.</p>"),
+      easyClose = TRUE,
+      footer = modalButton("Close")
+    ))
+  })
+  
+  observeEvent(input$helpButton, {
+    showModal(modalDialog(
+      title = "Q&A",
+      HTML("<p><b>What is balanced precision?</b></p>
+            <p>Balanced precision is useful in situations where the class distribution is imbalanced. In other words, it is the precision that would have been expected had the proportion of positive examples been balanced (equal to 50%).</p>
+           <p>Definition at https://doi.org/10.1016/j.ajhg.2021.08.012</p>"),
+      easyClose = TRUE,
+      footer = modalButton("Close")
+    ))
+  })
 }
 
 # Run the application
